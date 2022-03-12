c51dac82d48984cb309a492ecdf18120a826d76b
Maxim <m.skochinsky@mail.ru>
2022-03-12 19:27:23 +0300

    empty file

---
+++ LB1/Client-Server Application/CMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Server_Application
+{
+    [SerializableAttribute]
+    public class CMessage
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+
+        public CMessage()
+        {
+            Time = DateTime.Now;
+        }
+
+
+
+    }
+}
---
+++ LB1/Client-Server Application/CustomConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Server_Application
+{
+    public static class CustomConverter
+    {
+        public static byte[] Serialize(object anySerializableObject)
+        {
+            using(var memoryStream = new MemoryStream())
+            {
+                (new BinaryFormatter()).Serialize(memoryStream, anySerializableObject);
+                return memoryStream.ToArray();
+            }    
+        }
+
+        public static object Deserialize(byte[] data)
+        {
+            using(var memoryStream = new MemoryStream(data))
+            {
+                return (new BinaryFormatter()).Deserialize(memoryStream);
+            }
+        }
+    }
+}
